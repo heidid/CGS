e85fea2f20f53f9f6d2477952ef06d769e70f0d7
Simon Deng <simonsdeng@gmail.com>
2015-01-11 18:41:48 -0800

    Set up libGDX project

---
+++ desktop/src/com/hhsfbla/cgs/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.hhsfbla.cgs.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.hhsfbla.cgs.CGS;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CGS(), config);
+	}
+}
