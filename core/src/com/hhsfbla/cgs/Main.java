6d257c298e03df124c278ea288c04a459fd277c3
Simon Deng <simonsdeng@gmail.com>
2015-01-23 19:36:09 -0800

    Add missing StageScreen class and fix Viewport scaling

--- core/src/com/hhsfbla/cgs/Main.java
+++ core/src/com/hhsfbla/cgs/Main.java
@@ -1,15 +1,17 @@
 package com.hhsfbla.cgs;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.Scaling;
+import com.badlogic.gdx.utils.viewport.ScalingViewport;
 
 public class Main extends Game {
 	private Stage stage;
 
 	@Override
 	public void create() {
-		stage = new Stage();
+		stage = new Stage(new ScalingViewport(Scaling.fit, Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
 		setScreen(new LevelScreen(stage));
 	}
 }
