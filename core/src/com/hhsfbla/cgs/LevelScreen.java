ff877075e95fc3dc2dec7b7b5684dfe3d27ad3f7
Simon Deng <simonsdeng@gmail.com>
2015-01-30 20:52:52 -0800

    Remove extra imports

--- core/src/com/hhsfbla/cgs/LevelScreen.java
+++ core/src/com/hhsfbla/cgs/LevelScreen.java
@@ -4,7 +4,6 @@ import java.util.Comparator;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Stage;
