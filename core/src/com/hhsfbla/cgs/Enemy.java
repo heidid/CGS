67e86b001e55684430fa8041670b371d29744c60
Simon Deng <simonsdeng@gmail.com>
2015-01-31 00:36:13 -0800

    Make Enemy extend MovableActor

--- core/src/com/hhsfbla/cgs/Enemy.java
+++ core/src/com/hhsfbla/cgs/Enemy.java
@@ -1,12 +1,25 @@
 package com.hhsfbla.cgs;
 
+import java.util.TreeMap;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
-public class Enemy extends AnimatedActor {
+public class Enemy extends MovableActor {
 
 	public Enemy() {
-		super(new TextureRegion(new Texture(Gdx.files.internal("minion.png"))));
+		setIdleSprite(new TreeMap<Integer, Animation>() {{
+			put(DIR_UP, new Animation(0, new TextureRegion(
+					new Texture(Gdx.files.internal("minion-up.png")))));
+			put(DIR_DOWN, new Animation(0, new TextureRegion(
+					new Texture(Gdx.files.internal("minion-down.png")))));
+			put(DIR_LEFT, new Animation(0, new TextureRegion(
+					new Texture(Gdx.files.internal("minion-left.png")))));
+			put(DIR_RIGHT, new Animation(0, new TextureRegion(
+					new Texture(Gdx.files.internal("minion-right.png")))));
+		}});
+		setMoveSprite(getIdleSprite());
 	}
 }
