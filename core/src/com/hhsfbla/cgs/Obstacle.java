735f8e3b64fc85d2afcab2e1785ad8d4a3a06365
Simon Deng <simonsdeng@gmail.com>
2015-02-02 15:47:23 -0800

    Add oriented sprite constructors to Obstacle

--- core/src/com/hhsfbla/cgs/Obstacle.java
+++ core/src/com/hhsfbla/cgs/Obstacle.java
@@ -1,10 +1,11 @@
 package com.hhsfbla.cgs;
 
+import java.util.TreeMap;
+
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Obstacle extends AnimatedActor {
-
 	public Obstacle() {
 		super();
 	}
@@ -17,4 +18,16 @@ public class Obstacle extends AnimatedActor {
 		super(sprite);
 	}
 
+	public Obstacle(TreeMap<Integer, Animation> orientedSprite) {
+		super(orientedSprite);
+	}
+
+	public Obstacle(TreeMap<Integer, Animation> orientedSprite, int direction) {
+		super(orientedSprite, direction);
+	}
+
+	public Obstacle(TreeMap<Integer, Animation> orientedSprite, int direction,
+			float width, float height) {
+		super(orientedSprite, direction, width, height);
+	}
 }
