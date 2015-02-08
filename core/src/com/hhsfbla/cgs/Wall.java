8e51eae372f16ac0a3ecacd34aac42ad554f53a9
Simon Deng <simonsdeng@gmail.com>
2015-02-08 02:33:40 -0800

    Add vertical wall

--- core/src/com/hhsfbla/cgs/Wall.java
+++ core/src/com/hhsfbla/cgs/Wall.java
@@ -1,13 +1,30 @@
 package com.hhsfbla.cgs;
 
+import java.util.TreeMap;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Wall extends Obstacle {
+	// TODO: Fix wall bounding box
 
 	public Wall() {
-		super(new TextureRegion(new Texture(Gdx.files.internal("wall-h.png"))));
+		this(DIR_DOWN);
 	}
 
+	public Wall(int direction) {
+		super(new TreeMap<Integer, Animation>() {{
+			final Animation horizontal = new Animation(0, new TextureRegion(
+					new Texture(Gdx.files.internal("wall-h.png"))));
+			final Animation vertical = new Animation(0, new TextureRegion(
+					new Texture(Gdx.files.internal("wall-v.png"))));
+
+			put(DIR_UP, horizontal);
+			put(DIR_DOWN, horizontal);
+			put(DIR_LEFT, vertical);
+			put(DIR_RIGHT, vertical);
+		}}, direction);
+	}
 }
