06e6bf1e0be151fe7fbda767ff1536175df464c8
tom <tombousso@gmail.com>
2015-02-07 19:54:47 -0800

    Use xy not Vector2

--- core/src/com/hhsfbla/cgs/PathFindingAction.java
+++ core/src/com/hhsfbla/cgs/PathFindingAction.java
@@ -1,17 +1,17 @@
 package com.hhsfbla.cgs;
 
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
 
 public class PathFindingAction extends SequenceAction {
 
-	public PathFindingAction(Level level, Actor a, Vector2 fin, float durationEach) {
+	public PathFindingAction(Level level, Actor a, int x, int y, float durationEach) {
 		super();
-		CellPath cp = level.grid.getPath((int)a.getX(), (int)a.getY(), (int)fin.x, (int)fin.y);
+		CellPath cp = level.grid.getPath((int)a.getX(), (int)a.getY(), x, y);
 		for(Cell c : cp.array){
 			this.addAction(Actions.moveTo(c.x, c.y, durationEach));
+			System.out.println(c.x+","+c.y);
 		}
 	}
 	
