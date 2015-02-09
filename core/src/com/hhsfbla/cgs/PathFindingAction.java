62f573235ead67c8ebe305039140d3e3ec6c2555
tom <tombousso@gmail.com>
2015-02-08 16:02:31 -0800

    Combine move actions

--- core/src/com/hhsfbla/cgs/PathFindingAction.java
+++ core/src/com/hhsfbla/cgs/PathFindingAction.java
@@ -6,13 +6,27 @@ import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
 
 public class PathFindingAction extends SequenceAction {
 
-	public PathFindingAction(Level level, Actor a, int x, int y, float durationEach) {
+	public PathFindingAction(Level level, Actor a, int x, int y,
+			float durationEach) {
 		super();
-		CellPath cp = level.grid.getPath((int)a.getX(), (int)a.getY(), x, y);
-		for(Cell c : cp.array){
+		CellPath cp = level.grid.getPath((int) a.getX(), (int) a.getY(), x, y);
+		float oldX = a.getX();
+		float oldY = a.getY();
+		int i = -1;
+		while(true){
+			i++;
+			Cell c = cp.array.get(i);
+			if(i == cp.array.size - 1){
+				this.addAction(Actions.moveTo(c.x, c.y, durationEach));
+				break;
+			}
+			Cell n = cp.array.get(i + 1);
+			if(c.x - oldX == n.x - c.x && c.y - oldY == n.y - c.y)
+				continue;
+			oldX = c.x;
+			oldY = c.y;
 			this.addAction(Actions.moveTo(c.x, c.y, durationEach));
-			//System.out.println(c.x+","+c.y);
 		}
 	}
-	
+
 }
