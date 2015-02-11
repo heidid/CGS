ece95b4cdddde1c8a814cab20e81704ed6533f70
Simon Deng <simonsdeng@gmail.com>
2015-02-10 22:00:46 -0800

    Make Enemy use diagonal sprites

--- core/src/com/hhsfbla/cgs/Enemy.java
+++ core/src/com/hhsfbla/cgs/Enemy.java
@@ -10,7 +10,7 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 public class Enemy extends MovableActor {
 
 	public Enemy() {
-		
+
 		setIdleSprite(new TreeMap<Integer, Animation>() {{
 			put(DIR_UP, new Animation(0, new TextureRegion(
 					new Texture(Gdx.files.internal("minion-up.png")))));
@@ -20,6 +20,14 @@ public class Enemy extends MovableActor {
 					new Texture(Gdx.files.internal("minion-left.png")))));
 			put(DIR_RIGHT, new Animation(0, new TextureRegion(
 					new Texture(Gdx.files.internal("minion-right.png")))));
+			put(DIR_UP_LEFT, new Animation(0, new TextureRegion(
+					new Texture(Gdx.files.internal("minion-up-left.png")))));
+			put(DIR_UP_RIGHT, new Animation(0, new TextureRegion(
+					new Texture(Gdx.files.internal("minion-up-right.png")))));
+			put(DIR_DOWN_LEFT, new Animation(0, new TextureRegion(
+					new Texture(Gdx.files.internal("minion-down-left.png")))));
+			put(DIR_DOWN_RIGHT, new Animation(0, new TextureRegion(
+					new Texture(Gdx.files.internal("minion-down-right.png")))));
 		}});
 		setMoveSprite(getIdleSprite());
 	}
