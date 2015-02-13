426bfed8d566cc80a068425228d8eabead888f56
tom <tombousso@gmail.com>
2015-02-13 14:17:20 -0800

    Remove debug

--- core/src/com/hhsfbla/cgs/StageScreen.java
+++ core/src/com/hhsfbla/cgs/StageScreen.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class StageScreen extends ScreenAdapter {
@@ -19,12 +21,19 @@ public class StageScreen extends ScreenAdapter {
 	public void show() {
 		Gdx.input.setInputProcessor(stage);
 	}
-
+	ShapeRenderer sr = new ShapeRenderer();
 	@Override
 	public void render(float delta) {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		stage.act();
 		stage.draw();
+		/*sr.begin(ShapeType.Filled);
+		for(int i=0;i<Level.GRID_ROWS;i++)
+			sr.rectLine(0, i*this.getStage().getHeight()/Level.GRID_ROWS, this.getStage().getWidth(), i*this.getStage().getHeight()/Level.GRID_ROWS, 5);
+		for(int i=0;i<Level.GRID_COLS;i++)
+			sr.rectLine(i*this.getStage().getWidth()/Level.GRID_COLS, 0, i*this.getStage().getWidth()/Level.GRID_COLS, this.getStage().getHeight(), 5);
+		sr.flush();
+		sr.end();*/
 	}
 
 	@Override
