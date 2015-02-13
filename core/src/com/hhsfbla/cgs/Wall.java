ba90ef2083e5d164ba7ce56868999e8f3c8cc342
Simon Deng <simonsdeng@gmail.com>
2015-02-12 20:10:40 -0800

    Add horizontal and vertical Wall hitboxes

--- core/src/com/hhsfbla/cgs/Wall.java
+++ core/src/com/hhsfbla/cgs/Wall.java
@@ -6,10 +6,9 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 
 public class Wall extends Obstacle {
-	// TODO: Fix wall bounding box
-
 	public Wall() {
 		this(DIR_DOWN);
 	}
@@ -20,24 +19,38 @@ public class Wall extends Obstacle {
 			final Animation horizontal = new Animation(0, new TextureRegion(
 					new Texture(Gdx.files.internal("wall-h.png"))));
 			final Animation vertical = new Animation(0, new TextureRegion(
-					new Texture(Gdx.files.internal("wall-v.png"))));
-			final Animation topRight = new Animation(0, new TextureRegion(
-					new Texture(Gdx.files.internal("wall-corner-bl.png"))));
-			final Animation topLeft = new Animation(0, new TextureRegion(
-					new Texture(Gdx.files.internal("wall-corner-br.png"))));
-			final Animation bottomRight = new Animation(0, new TextureRegion(
-					new Texture(Gdx.files.internal("wall-corner-tl.png"))));
-			final Animation bottomLeft = new Animation(0, new TextureRegion(
-					new Texture(Gdx.files.internal("wall-corner-tr.png"))));
-
+					new Texture(Gdx.files.internal("wall-v-middle.png"))));
 			put(DIR_UP, horizontal);
 			put(DIR_DOWN, horizontal);
 			put(DIR_LEFT, vertical);
 			put(DIR_RIGHT, vertical);
-			put(DIR_UP_RIGHT, topRight);
-			put(DIR_UP_LEFT, topLeft);
-			put(DIR_DOWN_RIGHT, bottomRight);
-			put(DIR_DOWN_LEFT, bottomLeft);
-		}}, direction);
+			put(DIR_UP_RIGHT, new Animation(0, new TextureRegion(
+					new Texture(Gdx.files.internal("wall-corner-bl.png")))));
+			put(DIR_UP_LEFT, new Animation(0, new TextureRegion(
+					new Texture(Gdx.files.internal("wall-corner-br.png")))));
+			put(DIR_DOWN_RIGHT, new Animation(0, new TextureRegion(
+					new Texture(Gdx.files.internal("wall-corner-tl.png")))));
+			put(DIR_DOWN_LEFT, new Animation(0, new TextureRegion(
+					new Texture(Gdx.files.internal("wall-corner-tr.png")))));
+		}}, direction, new TreeMap<Integer, Vector2>() {{
+			final Vector2 horizontal = new Vector2(1, 0.25f);
+			final Vector2 vertical = new Vector2(0.25f, 1);
+			put(DIR_UP, horizontal);
+			put(DIR_DOWN, horizontal);
+			put(DIR_LEFT, vertical);
+			put(DIR_RIGHT, vertical);
+		}});
+		directionChanged();
+	}
+
+	@Override
+	protected void directionChanged() {
+		super.directionChanged();
+		final int dir = getDirection();
+		if (dir == DIR_UP || dir == DIR_DOWN) {
+			setOriginY(0.5f);
+		} else if (dir == DIR_LEFT || dir == DIR_RIGHT) {
+			setOriginY(-1);
+		}
 	}
 }
