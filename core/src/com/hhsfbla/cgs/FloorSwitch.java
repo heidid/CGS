096bcd0a9bc4c7cc90c8ad049a60c4385cdd5271
tom <tombousso@gmail.com>
2015-02-13 14:56:19 -0800

    Floor switch

---
+++ core/src/com/hhsfbla/cgs/FloorSwitch.java
@@ -0,0 +1,23 @@
+package com.hhsfbla.cgs;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class FloorSwitch extends Obstacle {
+	boolean pressed;
+	public FloorSwitch(float x, float y) {
+		super();
+		setX(x);
+		setY(y);
+		setPressed(false);
+	}
+	
+	public void setPressed(boolean pressed) {
+		this.pressed = pressed;
+		if (pressed)
+			setSprite(new TextureRegion(new Texture(Gdx.files.internal("ground-swtich-pressed.png"))));
+		else
+			setSprite(new TextureRegion(new Texture(Gdx.files.internal("ground-swtich.png"))));
+	}
+}
