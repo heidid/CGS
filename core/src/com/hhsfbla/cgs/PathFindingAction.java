4fd8c5c732ba8bd90a9b1c650200075125968207
tom <tombousso@gmail.com>
2015-02-16 11:53:35 -0800

    Fix pathfinding combining

--- core/src/com/hhsfbla/cgs/PathFindingAction.java
+++ core/src/com/hhsfbla/cgs/PathFindingAction.java
@@ -9,7 +9,7 @@ public class PathFindingAction extends SequenceAction {
 		CellPath cp = level.grid.getPath((int) a.getX(), (int) a.getY(), x, y);
 		float oldX = a.getX();
 		float oldY = a.getY();
-		int i = -1;
+		int i = 0;
 		while (true) {
 			i++;
 			Cell c = cp.array.get(i);
@@ -18,10 +18,11 @@ public class PathFindingAction extends SequenceAction {
 				return;
 			}
 			Cell n = cp.array.get(i + 1);
-			if (c.x - oldX == n.x - c.x && c.y - oldY == n.y - c.y) //combine multiple moves in the same direction
-				continue;
+			boolean cont = (c.x - oldX == n.x - c.x && c.y - oldY == n.y - c.y); //combine multiple moves in the same direction
 			oldX = c.x;
 			oldY = c.y;
+			if (cont)
+				continue;
 			addAction(new AnimatedMoveToAction(c.x, c.y));
 		}
 	}
