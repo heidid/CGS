4273b00e41c992bc73676d1f82b7c6607b3d9347
Simon Deng <simonsdeng@gmail.com>
2015-02-17 22:31:21 -0800

    Make FloorSwitch work

--- core/src/com/hhsfbla/cgs/FloorSwitch.java
+++ core/src/com/hhsfbla/cgs/FloorSwitch.java
@@ -5,19 +5,56 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class FloorSwitch extends Obstacle {
-	boolean pressed;
-	public FloorSwitch(float x, float y) {
-		super();
-		setX(x);
-		setY(y);
-		setPressed(false);
+	private static final TextureRegion offSprite = new TextureRegion(
+			new Texture(Gdx.files.internal("ground-switch.png")));
+	private static final TextureRegion onSprite = new TextureRegion(
+			new Texture(Gdx.files.internal("ground-switch-pressed.png")));
+
+	private boolean on;
+	private AnimatedActor presser;
+	private Obstacle link;
+
+	public FloorSwitch() {
+		setBlocked(false);
+		setOn(false);
 	}
-	
-	public void setPressed(boolean pressed) {
-		this.pressed = pressed;
-		if (pressed)
-			setSprite(new TextureRegion(new Texture(Gdx.files.internal("ground-switch-pressed.png"))));
-		else
-			setSprite(new TextureRegion(new Texture(Gdx.files.internal("ground-switch.png"))));
+
+	public boolean isOn() {
+		return on;
+	}
+
+	public void setOn(boolean on) {
+		this.on = on;
+		setSprite(on ? onSprite : offSprite);
+		if (on) {
+			// TODO: Do something with link
+		}
+	}
+
+	@Override
+	protected void updateSprite() {
+		super.updateSprite();
+	}
+
+	public AnimatedActor getPresser() {
+		return presser;
+	}
+
+	public void setPresser(AnimatedActor presser) {
+		if (this.presser == null && presser != null) setOn(!on);
+		this.presser = presser;
+	}
+
+	@Override
+	protected void resolveCollision(AnimatedActor actor) {
+		setPresser(actor);
+	}
+
+	@Override
+	public void act(float delta) {
+		super.act(delta);
+		if (presser != null && !presser.getHitbox().overlaps(getHitbox())) {
+			setPresser(null);
+		}
 	}
 }
