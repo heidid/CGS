a566b4cf5f22021dbaf73e2fc05806dae2cc3422
Heidi Dong <dong.heidi@gmail.com>
2015-02-21 20:34:47 -0800

    fix images path

--- core/src/com/hhsfbla/cgs/Images.java
+++ core/src/com/hhsfbla/cgs/Images.java
@@ -1,5 +1,9 @@
 package com.hhsfbla.cgs;
 
+import java.util.HashMap;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -9,7 +13,7 @@ public class Images {
 	/*static HashMap<String, TextureRegion> map = new HashMap<String, TextureRegion>();
 	public static TextureRegion get(String s) {
 		if (!map.containsKey(s)) {
-			map.put(s, new TextureRegion(new Texture(Gdx.files.internal(s))));
+			map.put(s, new TextureRegion(new Texture(Gdx.files.internal("images/"+s))));
 		}
 		return map.get(s);
 	}*/
@@ -18,7 +22,7 @@ public class Images {
 		s = s.substring(0, s.length() - 4);
 		return a.findRegion(s);
 	}
-	
+
 	public static Animation getAnimation(String format, int i, int f, float duration) {
 		Array<TextureRegion> keyFrames = new Array<TextureRegion>();
 		int inc = (f > i) ? 1 : -1;
