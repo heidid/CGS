88a06ce6bed942365290aca352353b6b96741cba
Simon Deng <simonsdeng@gmail.com>
2015-02-27 14:42:20 -0800

    Use unpacked images for now

--- core/src/com/hhsfbla/cgs/Images.java
+++ core/src/com/hhsfbla/cgs/Images.java
@@ -10,18 +10,18 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.Array;
 
 public class Images {
-	/*static HashMap<String, TextureRegion> map = new HashMap<String, TextureRegion>();
+	static HashMap<String, TextureRegion> map = new HashMap<String, TextureRegion>();
 	public static TextureRegion get(String s) {
 		if (!map.containsKey(s)) {
 			map.put(s, new TextureRegion(new Texture(Gdx.files.internal("images/"+s))));
 		}
 		return map.get(s);
-	}*/
-	static TextureAtlas a = new TextureAtlas("packed/atlas.atlas");
-	public static TextureRegion get(String s) {
-		s = s.substring(0, s.length() - 4);
-		return a.findRegion(s);
 	}
+//	static TextureAtlas a = new TextureAtlas("packed/atlas.atlas");
+//	public static TextureRegion get(String s) {
+//		s = s.substring(0, s.length() - 4);
+//		return a.findRegion(s);
+//	}
 
 	public static Animation getAnimation(String format, int i, int f, float duration) {
 		Array<TextureRegion> keyFrames = new Array<TextureRegion>();
