c864dddd69be69d8153fc289e188a813165ed750
Simon Deng <simonsdeng@gmail.com>
2015-03-03 20:07:03 -0800

    Add Port base class

--- core/src/com/hhsfbla/cgs/Level1.java
+++ core/src/com/hhsfbla/cgs/Level1.java
@@ -1,39 +1,30 @@
 package com.hhsfbla.cgs;
 
 public class Level1 extends Level {
-	Enemy enemy = new Enemy();
-
-	@Override
-	public void init() {
-	}
 
 	public Level1() {
 		setPlayerPosition(1, 6);
-		add(enemy, 2, 1);
+		add(new Port(), 0, 6);
 
-		for (int i = 0; i < 15; i++) {
-			add(new Wall(), i, 7);
-			add(new Wall(), i, 0);
-		}
+		final Enemy enemy = new Enemy();
+		enemy.addAction(new MoveToAttack(player.getX(), player.getY()));
+		add(enemy, 2, 1);
 
 		for (int i = 0; i < 11; i++) {
 			add(new Wall(), i, 5);
 			add(new Wall(), i, 2);
 		}
 
-		add(new Wall(Wall.DIR_UP_RIGHT), 15, 7);
-		for (int i = 0; i < 6; i++) add(new Wall(Wall.DIR_UP), 15, 1 + i);
-		add(new Wall(Wall.DIR_DOWN_RIGHT), 15, 0);
-
 		add(new Wall(Wall.DIR_UP_RIGHT), 11, 5);
 		for (int i = 0; i < 2; i++) add(new Wall(Wall.DIR_UP), 11, 3 + i);
 		add(new Wall(Wall.DIR_DOWN_RIGHT), 11, 2);
 
 		final File key = new File();
-		final Switch swtch = new FirewallSwitch(true);
+		final Switch swtch = new FirewallSwitch();
 		add(swtch, 13, 3);
 		add(new Firewall(swtch), 1, 1);
 		add(key, 13, 5);
-		enemy.addAction(new MoveToAttack(player.getX(), player.getY()));
+
+		add(new Port(), 0, 0);
 	}
 }
---
+++ core/src/com/hhsfbla/cgs/Port.java
@@ -0,0 +1,47 @@
+package com.hhsfbla.cgs;
+
+import java.util.TreeMap;
+
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.math.Vector2;
+
+public class Port extends Obstacle {
+	public Port() {
+		this(DIR_RIGHT);
+	}
+
+	public Port(int direction) {
+		setSprite(new TreeMap<Integer, Animation>() {{
+//			put(DIR_UP, Images.get("port-exit-down.png"));
+			put(DIR_DOWN, new Animation(0, Images.get("port-exit-down.png")));
+			put(DIR_LEFT, new Animation(0, Images.get("port-exit-left.png")));
+			put(DIR_RIGHT, new Animation(0, Images.get("port-exit-right.png")));
+		}});
+		setSize(new TreeMap<Integer, Vector2>(){{
+			put(DIR_UP, new Vector2(4/3f, 1));
+			put(DIR_DOWN, new Vector2(4/3f, 1));
+			put(DIR_LEFT, new Vector2(1, 4/3f));
+			put(DIR_RIGHT, new Vector2(1, 4/3f));
+		}});
+		setDirection(direction);
+		updateOrigin();
+	}
+
+	protected void updateOrigin() {
+		switch (getDirection()) {
+		case DIR_DOWN:
+			setOriginY(1/2f);
+			break;
+		case DIR_LEFT:
+		case DIR_RIGHT:
+			setOriginY(11/12f);
+			break;
+		}
+	}
+
+	@Override
+	protected void directionChanged() {
+		super.directionChanged();
+		updateOrigin();
+	}
+}
