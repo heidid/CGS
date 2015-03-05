51849767982ba436081eb38062e0e8cb282cf9c1
Simon Deng <simonsdeng@gmail.com>
2015-03-05 01:19:41 -0800

    Add Pokemon Ice Gym-esque level using FloorSwitches

--- core/src/com/hhsfbla/cgs/Level2.java
+++ core/src/com/hhsfbla/cgs/Level2.java
@@ -1,30 +1,46 @@
 package com.hhsfbla.cgs;
 
 public class Level2 extends Level {
-	Enemy enemy = new Enemy();
-
-	@Override
-	public void init() {
-		enemy.addAction(new PathfindingAction(0, 0, 1));
-	}
-
 	public Level2() {
-		setPlayerPosition(1, 7);
-		add(enemy, 1, 1);
+		add(new PlayerSpawn(PlayerSpawn.DIR_DOWN), 2, 7);
 
-		for (int i = 0; i < 5; i++) {
-			final int dir = i == 0 ? Wall.DIR_LEFT : Wall.DIR_RIGHT;
-			final boolean end = i % 4 == 0;
+		add(new Wall(Wall.DIR_UP), 1, 6);
+		add(new Wall(Wall.DIR_DOWN_LEFT), 3, 6);
+		add(new Wall(Wall.DIR_UP_RIGHT), 4, 6);
+		add(new Wall(Wall.DIR_DOWN_RIGHT), 1, 5);
+		add(new Wall(Wall.DIR_UP_LEFT), 0, 5);
+		add(new Wall(Wall.DIR_UP), 4, 5);
+		add(new Wall(Wall.DIR_UP), 0, 4);
+		add(new Wall(Wall.DIR_UP), 4, 4);
+		add(new Wall(Wall.DIR_UP), 0, 3);
+		add(new Wall(Wall.DIR_DOWN_RIGHT), 4, 3);
+		add(new Wall(Wall.DIR_UP_LEFT), 3, 3);
+		add(new Wall(Wall.DIR_DOWN_LEFT), 0, 2);
+		add(new Wall(Wall.DIR_UP_RIGHT), 1, 2);
+		add(new Wall(Wall.DIR_DOWN), 3, 2);
+		add(new Wall(Wall.DIR_DOWN, true), 1, 1);
+		add(new Wall(Wall.DIR_DOWN, true), 3, 1);
 
-			add(new Wall(dir, end), 6 + i, 7);
-			add(new Wall(dir, end), 6 + i, 5);
-			add(new Wall(dir, end), 6 + i, 3);
-			add(new Wall(dir, end), 6 + i, 1);
-		}
+		final FloorSwitch[] switches = {
+				new FloorSwitch(),
+				new FloorSwitch(),
+				new FloorSwitch(),
+				new FloorSwitch(),
+				new FloorSwitch(),
+				new FloorSwitch(),
+				new FloorSwitch()
+		};
 
-		for (int i = 0; i < 7; i++) {
-			add(new Wall(i == 0 ? Wall.DIR_DOWN : Wall.DIR_UP,
-					i % 6 == 0), 12, 1 + i);
-		}
+		add(switches[0], 2, 5);
+		add(switches[1], 3, 5);
+		add(switches[2], 1, 4);
+		add(switches[3], 2, 4);
+		add(switches[4], 3, 4);
+		add(switches[5], 1, 3);
+		add(switches[6], 2, 3);
+
+		add(new SwitchGroupDoor(switches), 2, 1);
+
+		add(new ExitPort(ExitPort.DIR_LEFT), 15, 0);
 	}
 }
