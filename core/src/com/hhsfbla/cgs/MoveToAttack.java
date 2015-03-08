e20de294cd31666f11939cea70b3d314c68900a9
tom <tombousso@gmail.com>
2015-03-08 12:55:55 -0700

    MoveToAttack no diagonals

--- core/src/com/hhsfbla/cgs/MoveToAttack.java
+++ core/src/com/hhsfbla/cgs/MoveToAttack.java
@@ -18,7 +18,7 @@ public class MoveToAttack extends SequenceAction {
 		int len = Integer.MAX_VALUE;
 		for(int i = (int) (x - 1); i <= (int) (x + 1); i++) {
 			for(int j = (int) (y - 1); j <= (y + 1); j++) {
-				if(i == x && j == y || i < 0 || j < 0 || i >= Level.GRID_ROWS || j >= Level.GRID_COLS)
+				if((i == x && j == y) || (i != x && j != y) || i < 0 || j < 0 || i >= Level.GRID_ROWS || j >= Level.GRID_COLS)
 					continue;
 				final CellPath cp = a.getLevel().grid.getPath(
 						(int) a.getX(), (int) a.getY(), i, j);
