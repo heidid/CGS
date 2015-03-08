d51b12d5f29e7c77b6efb867b6cf2b150e2d0701
Heidi Dong <dong.heidi@gmail.com>
2015-03-08 14:03:34 -0700

    animated left conveyor belts

--- core/src/com/hhsfbla/cgs/ConveyorBelt.java
+++ core/src/com/hhsfbla/cgs/ConveyorBelt.java
@@ -38,7 +38,7 @@ public class ConveyorBelt extends Obstacle {
 		setSprite(new TreeMap<Integer, Animation>() {{
 			put(DIR_UP, new Animation(0, Images.get("conveyor-belt-up.png")));
 			put(DIR_DOWN, new Animation(0, Images.get("conveyor-belt-down.png")));
-			put(DIR_LEFT, new Animation(0, Images.get("conveyor-belt-left.png")));
+			put(DIR_LEFT, Images.getAnimation("conveyorbelt-left-%d.png", 24, 0, 0.05f, PlayMode.LOOP));
 			put(DIR_RIGHT, Images.getAnimation("conveyorbelt-%d.png", 24, 0, 0.05f, PlayMode.LOOP));
 			put(DIR_UP_LEFT, new Animation(0, Images.get("conveyor-belt-br.png")));
 			put(DIR_UP_RIGHT, new Animation(0, Images.get("conveyor-belt-bl.png")));
