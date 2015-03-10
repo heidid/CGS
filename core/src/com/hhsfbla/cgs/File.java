cf1e59791226dbfe35a84830e2f234fcd113a052
Heidi Dong <dong.heidi@gmail.com>
2015-03-09 22:16:48 -0700

    size of file is actually a really nice fraction

--- core/src/com/hhsfbla/cgs/File.java
+++ core/src/com/hhsfbla/cgs/File.java
@@ -1,8 +1,15 @@
 package com.hhsfbla.cgs;
 
 public class File extends Item {
+
 	public File() {
 		setSprite(Images.get("file.png"));
-		setSize(2/3f, 5/6f);
+		setSize(3/5f, 4/5f);
+	}
+
+	// n = number of asterisks
+	public File(int n) {
+		setSprite(Images.get("file-pw"+n+".png"));
+		setSize(3/5f, 4/5f);
 	}
 }
