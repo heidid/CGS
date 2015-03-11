e39a4b8056e83e846dbde6c9181041790251832a
Heidi Dong <dong.heidi@gmail.com>
2015-03-10 17:14:48 -0700

    port [dis]appear animations

--- core/src/com/hhsfbla/cgs/SpawnPort.java
+++ core/src/com/hhsfbla/cgs/SpawnPort.java
@@ -8,16 +8,16 @@ public class SpawnPort extends Port {
 	public SpawnPort() {
 		this(DIR_RIGHT);
 		setAppearSprite(new TreeMap<Integer, Animation>() {{
-			//put(DIR_UP, new Animation(0, Images.get("port-exit-down.png")));
+			put(DIR_UP, Images.getAnimation("port-enemy-up-%d.png", 0, 17, 0.05f));
 			put(DIR_DOWN, Images.getAnimation("port-enemy-down-%d.png", 0, 8, 0.05f));
-			//put(DIR_LEFT, new Animation(0, Images.get("port-exit-left.png")));
-			//put(DIR_RIGHT, new Animation(0, Images.get("port-exit-right.png")));
+			put(DIR_LEFT, Images.getAnimation("port-enemy-left-%d.png", 0, 14, 0.05f));
+			put(DIR_RIGHT, Images.getAnimation("port-enemy-right-%d.png", 0, 14, 0.05f));
 		}});
 		setDisappearSprite(new TreeMap<Integer, Animation>() {{
-			//put(DIR_UP, new Animation(0, Images.get("port-exit-down.png")));
+			put(DIR_UP, Images.getAnimation("port-enemy-up-%d.png", 17, 0, 0.05f));
 			put(DIR_DOWN, Images.getAnimation("port-enemy-down-%d.png", 8, 0, 0.05f));
-			//put(DIR_LEFT, new Animation(0, Images.get("port-exit-left.png")));
-			//put(DIR_RIGHT, new Animation(0, Images.get("port-exit-right.png")));
+			put(DIR_LEFT, Images.getAnimation("port-enemy-left-%d.png", 14, 0, 0.05f));
+			put(DIR_RIGHT, Images.getAnimation("port-enemy-right-%d.png", 14, 0, 0.05f));
 		}});
 	}
 
