5e5ea437429c57b2cdcd19b210df22e5fbb788d3
Heidi Dong <dong.heidi@gmail.com>
2015-03-18 23:21:51 -0700

    diagonal [dis]appear player sprites

--- core/src/com/hhsfbla/cgs/Player.java
+++ core/src/com/hhsfbla/cgs/Player.java
@@ -32,26 +32,25 @@ public class Player extends MovableActor {
 			put(DIR_DOWN_RIGHT, new Animation(0, Images.get("player-down-right.png")));
 		}});
 		setMoveSprite(getIdleSprite());
-		// TODO: Add actual player appear/disappear sprites
 		setAppearSprite(new TreeMap<Integer, Animation>() {{
 			put(DIR_UP, Images.getAnimation("player-up-%d.png", 9, 0, 0.03f));
 			put(DIR_DOWN, Images.getAnimation("player-down-%d.png", 9, 0, 0.03f));
 			put(DIR_LEFT, Images.getAnimation("player-left-%d.png", 9, 0, 0.03f));
 			put(DIR_RIGHT, Images.getAnimation("player-right-%d.png", 9, 0, 0.03f));
-			put(DIR_UP_LEFT, Images.getAnimation("minion-up-left-die-%d.png", 0, 9, 0.03f));
-			put(DIR_UP_RIGHT, Images.getAnimation("minion-up-right-%d.png", 0, 9, 0.03f));
-			put(DIR_DOWN_LEFT, Images.getAnimation("minion-down-left-%d.png", 0, 9, 0.03f));
-			put(DIR_DOWN_RIGHT, Images.getAnimation("minion-down-right-%d.png", 0, 9, 0.03f));
+			put(DIR_UP_LEFT, Images.getAnimation("player-up-left-%d.png", 9, 0, 0.03f));
+			put(DIR_UP_RIGHT, Images.getAnimation("player-up-right-%d.png", 9, 0, 0.03f));
+			put(DIR_DOWN_LEFT, Images.getAnimation("player-down-left-%d.png", 9, 0, 0.03f));
+			put(DIR_DOWN_RIGHT, Images.getAnimation("player-down-right-%d.png", 9, 0, 0.03f));
 		}});
 		setDisappearSprite(new TreeMap<Integer, Animation>() {{
 			put(DIR_UP, Images.getAnimation("player-up-%d.png", 0, 9, 0.03f));
 			put(DIR_DOWN, Images.getAnimation("player-down-%d.png", 0, 9, 0.03f));
 			put(DIR_LEFT, Images.getAnimation("player-left-%d.png", 0, 9, 0.03f));
 			put(DIR_RIGHT, Images.getAnimation("player-right-%d.png", 0, 9, 0.03f));
-			put(DIR_UP_LEFT, Images.getAnimation("minion-up-left-die-%d.png", 9, 0, 0.03f));
-			put(DIR_UP_RIGHT, Images.getAnimation("minion-up-right-%d.png", 9, 0, 0.03f));
-			put(DIR_DOWN_LEFT, Images.getAnimation("minion-down-left-%d.png", 9, 0, 0.03f));
-			put(DIR_DOWN_RIGHT, Images.getAnimation("minion-down-right-%d.png", 9, 0, 0.03f));
+			put(DIR_UP_LEFT, Images.getAnimation("player-up-left-%d.png", 0, 9, 0.03f));
+			put(DIR_UP_RIGHT, Images.getAnimation("player-up-right-%d.png", 0, 9, 0.03f));
+			put(DIR_DOWN_LEFT, Images.getAnimation("player-down-left-%d.png", 0, 9, 0.03f));
+			put(DIR_DOWN_RIGHT, Images.getAnimation("player-down-right-%d.png", 0, 9, 0.03f));
 		}});
 
 		setOrigin(Align.bottom);
