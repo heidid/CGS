264f97709938eb00f83f4e422e4d4b6adaebfcfb
Simon Deng <simonsdeng@gmail.com>
2015-03-27 14:09:28 -0700

    Adjust File hitbox

--- core/src/com/hhsfbla/cgs/File.java
+++ core/src/com/hhsfbla/cgs/File.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.scenes.scene2d.utils.Align;
 public class File extends Item {
 	public File() {
 		setSprite(Images.get("file.png"));
-		setSize(3/5f, 4/5f);
+		setSize(3/5f, 3/5f);
 		setOrigin(Align.bottom);
 	}
 }
