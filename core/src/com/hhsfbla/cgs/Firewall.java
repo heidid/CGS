ec1cdf6b0f7a089cce756357139b474c29c54f0a
Heidi Dong <dong.heidi@gmail.com>
2015-03-26 18:03:39 -0700

    blue, green, and purple Firewalls, and small flame

--- core/src/com/hhsfbla/cgs/Firewall.java
+++ core/src/com/hhsfbla/cgs/Firewall.java
@@ -1,6 +1,11 @@
 package com.hhsfbla.cgs;
 
 public class Firewall extends UnblockableObstacle implements SwitchListener {
+
+	public static final String BLUE = "blue";
+	public static final String GREEN = "green";
+	public static final String PURPLE = "purple";
+
 	public Firewall(Switch swtch) {
 		this(true, swtch);
 	}
@@ -9,8 +14,19 @@ public class Firewall extends UnblockableObstacle implements SwitchListener {
 		super(blocked);
 		setBlockedSprite(Images.get("firewall.png"));
 		setUnblockedSprite(Images.get("firewall-disabled.png"));
-		setBlockingAnimation(Images.getAnimation("firewall-appear-%d.png", 1, 4, 0.03f));
-		setUnblockingAnimation(Images.getAnimation("firewall-appear-%d.png", 4, 1, 0.03f));
+		setBlockingAnimation(Images.getAnimation("firewall-%d.png", 9, 0, 0.03f));
+		setUnblockingAnimation(Images.getAnimation("firewall-%d.png", 0, 9, 0.03f));
+		swtch.addSwitchListener(this);
+		setSize(1, 1/6f);
+		setOriginY(5/12f);
+	}
+
+	public Firewall(boolean blocked, Switch swtch, String color) {
+		super(blocked);
+		setBlockedSprite(Images.get("firewall-"+color+".png"));
+		setUnblockedSprite(Images.get("firewall-disabled.png"));
+		setBlockingAnimation(Images.getAnimation("firewall-"+color+"-%d.png", 9, 0, 0.03f));
+		setUnblockingAnimation(Images.getAnimation("firewall-"+color+"-%d.png", 0, 9, 0.03f));
 		swtch.addSwitchListener(this);
 		setSize(1, 1/6f);
 		setOriginY(7/12f);
