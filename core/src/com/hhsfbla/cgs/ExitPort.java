4edf430d3063d5adf67e9905a12cae89ae131a22
Simon Deng <simonsdeng@gmail.com>
2015-03-27 00:18:48 -0700

    Fix up-facing ExitPort animations

--- core/src/com/hhsfbla/cgs/ExitPort.java
+++ core/src/com/hhsfbla/cgs/ExitPort.java
@@ -19,13 +19,13 @@ public class ExitPort extends Port {
 			put(DIR_RIGHT, new Animation(0, Images.get("port-exit-right.png")));
 		}});
 		setAppearAnimation(new TreeMap<Integer, Animation>() {{
-			put(DIR_UP, Images.getAnimation("port-exit-up-%d.png", 0, 17, 0.05f));
+			put(DIR_UP, Images.getAnimation("port-exit-up-%d.png", 17, 0, 0.05f));
 			put(DIR_DOWN, Images.getAnimation("port-exit-down-%d.png", 8, 0, 0.05f));
 			put(DIR_LEFT, Images.getAnimation("port-exit-left-%d.png", 14, 0, 0.05f));
 			put(DIR_RIGHT, Images.getAnimation("port-exit-right-%d.png", 14, 0, 0.05f));
 		}});
 		setDisappearAnimation(new TreeMap<Integer, Animation>() {{
-			put(DIR_UP, Images.getAnimation("port-exit-up-%d.png", 17, 0, 0.05f));
+			put(DIR_UP, Images.getAnimation("port-exit-up-%d.png", 0, 17, 0.05f));
 			put(DIR_DOWN, Images.getAnimation("port-exit-down-%d.png", 0, 8, 0.05f));
 			put(DIR_LEFT, Images.getAnimation("port-exit-left-%d.png", 0, 14, 0.05f));
 			put(DIR_RIGHT, Images.getAnimation("port-exit-right-%d.png", 0, 14, 0.05f));
